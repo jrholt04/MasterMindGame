e179d0b1645b70be16ad6e7330417c36bf6cbee5
jrholt26 <jrholt26@transy.edu>
2024-01-16 18:08:43 -0500

    this is a commit of the incr 001.1

--- MasterMindGame/ContentView.swift
+++ MasterMindGame/ContentView.swift
@@ -1,16 +1,43 @@
+//File: ContentView
+//Jackson Holt
+//CS 3164, Winter 2024
 //
 //  ContentView.swift
 //  MasterMindGame
 //
 //  Created by Jackson Holt on 1/16/24.
+// this is the view file for what the master mind game will look like
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            
+        HStack {
+            ZStack{
+                Circle().foregroundColor(.red)
+                Circle().stroke(lineWidth: 3)
+            }
+            ZStack{
+                Circle().foregroundColor(.orange)
+                Circle().stroke(lineWidth: 3)
+            }
+            ZStack{
+                Circle().foregroundColor(.yellow)
+                Circle().stroke(lineWidth: 3)
+            }
+            ZStack{
+                Circle().foregroundColor(.green)
+                Circle().stroke(lineWidth: 3)
+            }
+            ZStack{
+                Circle().foregroundColor(.blue)
+                Circle().stroke(lineWidth: 3)
+            }
+            ZStack{
+                Circle().foregroundColor(.purple)
+                Circle().stroke(lineWidth: 3)
+            }
         }
         .padding()
     }
