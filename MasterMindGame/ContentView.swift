fb43fae2c58c3e90c4f16716369ec9b4cce4f55b
jrholt26 <jrholt26@transy.edu>
2024-01-16 15:30:13 -0500

    Initial Commit

---
+++ MasterMindGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MasterMindGame
+//
+//  Created by Jackson Holt on 1/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MasterMindGame/MasterMindGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterMindGameApp.swift
+//  MasterMindGame
+//
+//  Created by Jackson Holt on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MasterMindGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
