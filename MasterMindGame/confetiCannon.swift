693e7cc01ba222fbb20e1cc490bb7b549eae4d92
jrholt26 <jrholt26@transy.edu>
2024-03-25 15:40:27 -0400

    adds the package for the effects library as well as a win and loss conferti action that plays on win or loss of the game and switches the reset button to the win and loss overlays

---
+++ MasterMindGame/confetiCannon.swift
@@ -0,0 +1,18 @@
+//
+//  confetiCannon.swift
+//  MasterMindGame
+//
+//  Created by Jackson Holt on 3/25/24.
+//
+
+import SwiftUI
+
+struct confetiCannon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    confetiCannon()
+}
