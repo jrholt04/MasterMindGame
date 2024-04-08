52d146b7c7d07331b4211e819649e5df337ab2eb
jrholt26 <jrholt26@transy.edu>
2024-04-08 13:18:25 -0400

    Basic Naviagtion added through home page

---
+++ MasterMindGame/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  MasterMindGame
+//
+//  Created by Jackson Holt on 4/8/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ MasterMindGame/SettingView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingView.swift
+//  MasterMindGame
+//
+//  Created by Jackson Holt on 4/8/24.
+//
+
+import Foundation
